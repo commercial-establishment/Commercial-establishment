ec827364fd9478efdb72e1db3f8bc38d6093152a
alikhann <alikhannugmanov@gmail.com>
2015-10-28 18:08:31 +0600

    Global Provider changes

---
+++ src/main/java/kz/hts/ce/service/ProductService.java
@@ -0,0 +1,22 @@
+package kz.hts.ce.service;
+
+import kz.hts.ce.entity.Product;
+import kz.hts.ce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService extends BaseService<Product, ProductRepository>{
+
+    @Autowired
+    protected ProductService(ProductRepository repository) {
+        super(repository);
+    }
+
+    public List<Product> findByProducts(String product) {
+
+        return repository.findByProducts(product);
+    }
+}
