f94e16a5f0a220edcab95115c358a739c3539311
yakovshmidt <yakovshmidt93@gmail.com>
2015-12-31 09:44:47 +0600

    Added annotation @Column(nullable = false)

--- src/main/java/kz/hts/ce/entity/Category.java
+++ src/main/java/kz/hts/ce/entity/Category.java
@@ -2,6 +2,7 @@ package kz.hts.ce.entity;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.Size;
 
@@ -10,6 +11,7 @@ public class Category extends BaseEntity {
 
     @NotEmpty
     @Size(min = 2, max = 20)
+    @Column(nullable = false)
     private String name;
 
     public String getName() {
