9abe14e6bf3172bba28bf7f27cffdeb9cb0c7566
yakovshmidt <yakovshmidt93@gmail.com>
2016-01-05 13:29:08 +0600

    Added findByUsernameAndBlocked method

--- src/main/java/kz/hts/ce/repository/EmployeeRepository.java
+++ src/main/java/kz/hts/ce/repository/EmployeeRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
     Employee findByUsername(String username);
+    Employee findByUsernameAndBlocked(String username, boolean blocked);
 }
